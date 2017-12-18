367fa9096160eb48bf0926a413c590e5662e55c6
Volha_Bystrova <volha_bystrova@epam.com>
2017-12-18 12:55:20 +0300

    New structure, added SASS

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+webpackConfig = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: [/node_modules/],
+                /*loader: "babel-loader",
+                query: {
+                    presets: ['es2015', 'react', 'stage-0', 'stage-1']
+                }*/
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+            },
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css', {
+            allChunks: true
+        })
+    ]
+};
+module.exports = webpackConfig;
